43c55ee8892b679511f45788d4f91520b30fcebb
RyanPeking <wushunyan@163.com>
2019-05-05 22:39:46 +0800

    2019.2.7

---
+++ 爬虫与实战/etree糗百.py
@@ -0,0 +1,34 @@
+'''
+爬取糗事百科，页面自己来找
+分析:
+1.需要用到request爬取页面，用xpath、re来提取数字
+2.可提取信息用户头像链接，段子内容，点赞，好评次数
+3.保存在json文件中
+
+大致分三部分
+1.down下页面
+2.利用xpath提取信息
+3.保存文件落地
+'''
+
+import requests
+from lxml import etree
+
+url = "https://www.qiushibaike.com/8hr/page/1/"
+headers = {
+    "Accept-Language":"zh-CN,zh;q=0.9",
+    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36",
+    }
+rsp = requests.get(url, headers=headers)
+html = rsp.text
+
+html = etree.HTML(html)
+rst = html.xpath('//li[@class="item typs_video"]')#contains(@id, "qiushi_tag")
+i = 0
+for r in rst:
+
+    item = {}
+    print(type(r))
+    content = r.xpath('//a[@class="recmd-content"]')[i].text
+    i += 1
+    print(content)
---
+++ 爬虫与实战/扇贝.py
@@ -0,0 +1,12 @@
+'''
+扇贝单词
+1.把python单词列表download下来
+2.主要练习目的xpath
+3.理论上不需要登录
+4.https://www.shanbay.com/wordbook/187711/
+'''
+
+from urllib import request
+from lxml import etree
+
+import json
