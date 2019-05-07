1247b4149ec159396eb28cef7ff492a0dba9b9d7
RyanPeking <wushunyan@163.com>
2019-05-07 22:30:24 +0800

    2019.2.7

---
+++ 爬虫与实战/e15_meiju/e15_meiju/spiders/meituspider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+from e15_meiju.items import MeijuItem
+
+class MeijuSpider(scrapy.Spider):
+
+    name = "meiju"
+
+    start_urls = ['http://www.meijutt.com/new100.html']
+
+    def parse(self, response):
+        movies = response.xpath('//ul[@class="top-list  fn-clear"]/li')
+
+        for movie in movies:
+            item = MeijuItem()
+            item['name'] = movie.xpath('./h5/a/@title').extract()[0]
+            item['href'] = movie.xpath('./h5/a/@href').extract()[0]
+
+            tv = movie.xpath('./span[@class="mjtv"]/text()')
+
+            if len(tv):
+                item['tv'] = tv.extract()[0]
+            else:
+                item['tv'] = ""
+            print('Item.name:{0}'.format(item['name']))
+            yield item
