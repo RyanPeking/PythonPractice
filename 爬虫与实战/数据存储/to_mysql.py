9d261b5ffe7a367859bba1a3151458ab332b3c9b
RyanPeking <wushunyan@163.com>
2019-05-14 22:40:15 +0800

    2019.2.7

--- 爬虫与实战/数据存储/to_mysql.py
+++ 爬虫与实战/数据存储/to_mysql.py
@@ -13,7 +13,7 @@ Ubantu环境安装mysql
     http://www.cnblogs.com/guozhiping/p/7684134.html
 
 mysql数据库远程连通：
-    1.修改/etc/mysql/my.conf
+    1.修改/etc/mysql/mysql.conf.d/mysqld.cnf
         找到bind-address = 127.0.0.1 这一行，将这一行注释
         或者：将这一行改为bind-address = 0.0.0.0
 
@@ -33,65 +33,70 @@ Python操作mysql
 
 # Python操作mysql创建数据表
 import pymysql
-
+#
 # try:
 #     # 获取一个数据库连接，注意：如果是utf-8类型，需要定制数据库
 #     # 打开数据库连接
-#     con = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db='ceshi', port=3306)
+#     db = pymysql.connect(host='127.0.0.1', user='root', passwd='123456', db='ceshi', port=3306)
 #     # 创建游标，对数据库进行操作，使用cursor()方法
 #     # print(con)
-#     cur = con.cursor()
+#     cursor = db.cursor()
 #     # 使用execute（）执行sql语句
-#     # cur.execute('DROP TABLES IF EXISTS BJTLXY')
+#     # cursor.execute('DROP TABLES IF EXISTS BJTLXY')
 #     # 使用预处理语句创建表
-#     sql = """CREATE TABLE IF NOT EXISTS BJTLXY(
+#     sql = "CREATE TABLE IF NOT EXISTS BJTLXY(
 #     FIRST_NAME CHAR(20) NOT NULL,
 #     LAST_NAME CHAR(20),
 #     AGE INT,
 #     SEX CHAR(1),
-#     INCOME FLOAT)"""
-#     cur.execute(sql)
-#     con.close()
+#     INCOME FLOAT)"
+#     cursor.execute(sql)
+#     db.close()
 # except:
 #     print("创建失败")
 
-
-# 数据库插入操作
-db = pymysql.connect('127.0.0.1', 'root', '123456', 'ceshi')
-# 利用cursor()创建游标对象
-cursor = db.cursor()
-
-# sql语句
-sql = 'insert into BJTLXY(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)VALUES("Liu2", "DANA", 18, "M", "100000"),("HUANG", "LAOBAN", 19, "M", "99999")'
-# 为了防止sql注入
-# sql = "INSERT INTO BJTLXY(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) \
-#       VALUE('%s', '%s', '%d', '%c', '%s')"%('Liu', 'DANA', 18, 'M', '100000')
-
-try:
-    # 执行sql语句
-    cursor.execute(sql)
-    # 提交执行
-    db.commit()
-    print('执行成功')
-except:
-    db.rollback()
-    print('执行失败')
-db.close()
-
-
 #
-# import pymysql
-# import sys
-# con=pymysql.connect('127.0.0.1','root','123456','ceshi')
-# # with con:
+# # 数据库插入操作
+# db = pymysql.connect('127.0.0.1', 'root', '123456', 'ceshi')
+# # 利用cursor()创建游标对象
+# cursor = db.cursor()
+
+# # sql语句
+# sql = 'insert into BJTLXY(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME)VALUES("Liu2", "DANA", 18, "M", "100000"),("HUANG", "LAOBAN", 19, "M", "99999")'
+# # 为了防止sql注入
+# # sql = "INSERT INTO BJTLXY(FIRST_NAME, LAST_NAME, AGE, SEX, INCOME) \
+# #       VALUE('%s', '%s', '%d', '%c', '%s')"%('Liu', 'DANA', 18, 'M', '100000')
+#
 # try:
-#     cur=con.cursor()
-#     sql = "CREATE TABLE IF NOT EXISTS BJTLXY(FIRST_NAME CHAR(20) NOT NULL)"
-#     cur.execute(sql)
-#     # cur.execute("INSERT INTO Writers(Name) VALUES('Jack London')")
-#     # cur.execute("INSERT INTO Writers(Name) VALUES('hONORE DE bALZAC')")
-#     # cur.execute("INSERT INTO Writers(Name) VALUES('Lion Feuchtwanger')")
-#     # cur.execute("INSERT INTO Writers(Name) VALUES('Emile Zola')")
-#     # cur.execute("INSERT INTO Writers(Name) VALUES('Truman Capote')")
+#     # 执行sql语句
+#     cursor.execute(sql)
+#     # 提交执行
+#     db.commit()
+#     print('执行成功')
 # except:
-#     print('1')
+#     db.rollback()
+#     print('执行失败')
+# db.close()
+
+# 数据库查询操作
+try:
+    db = pymysql.connect('127.0.0.1', 'root', '123456', 'ceshi')
+    cursor = db.cursor()
+    cursor.execute('select * from BJTLXY')
+    datas = cursor.fetchall()
+    rows = cursor.rowcount
+    print(rows)
+    for data in datas:
+        print(data)
+    cursor.close()
+    db.close()
+except Exception as e:
+    print("查询失败")
+    print(e)
+
+
+'''
+fetchall():接受全部返回结果
+fetchone():获取下一个查询结果集
+rowcount:只读属性，返回执行语句影响的行数
+'''
